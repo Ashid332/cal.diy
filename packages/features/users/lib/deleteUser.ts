9eab07e64607f9a1200c492c8a290ae895e06335
Ashidul Islam <ashiduli53@gmail.com>
2026-07-17 11:25:49 +0530

    fix(users): pass prisma to UserRepository constructor

--- packages/features/users/lib/deleteUser.ts
+++ packages/features/users/lib/deleteUser.ts
@@ -1,11 +1,12 @@
 import { deleteStripeCustomer } from "@calcom/app-store/stripepayment/lib/customer";
 import type { User } from "@calcom/prisma/client";
+import prisma from "@calcom/prisma";
 import { UserRepository } from "@calcom/features/users/repositories/UserRepository";
 
 export async function deleteUser(user: Pick<User, "id" | "email" | "metadata">) {
   // If 2FA is disabled or totpCode is valid then delete the user from stripe and database
   await deleteStripeCustomer(user).catch(console.warn);
   // Remove my account
-  const userRepository = new UserRepository();
+  const userRepository = new UserRepository(prisma);
   await userRepository.delete({ id: user.id });
 }
